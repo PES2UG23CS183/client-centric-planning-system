2043096eb162180f1744800e93c6f72e06c70e26
PES2UG23CS183 <pes2ug23cs183@pesu.pes.edu>
2026-04-07 16:27:15 +0530

    Diya - main application

---
+++ src/main/java/com/clientplanning/ClientPlanningApplication.java
@@ -0,0 +1,11 @@
+package com.clientplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPlanningApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientPlanningApplication.class, args);
+    }
+}
